cf57f2e0cabc1ac51ad55b93f4450514d6f072f1
Nurdeen Nurmanbetov <nurdin.nurmanbetov@gmail.com>
2021-02-25 23:56:38 -0500

    zeros for heros fixed

--- HW_Algorithms_2/HW_Algorithms_2.py
+++ HW_Algorithms_2/HW_Algorithms_2.py
@@ -17,13 +17,25 @@ def fib(num):
 
 
 # Zeros for heroes
+    # Takes out all the zeros
+# def zeros(num):
+#     num = str(num)[::-1]
+#     result = ''
+#     for digit in num:
+#         if int(digit) != 0:
+#             result += digit
+#     return int(result[::-1])
 def zeros(num):
-    num = str(num)[::-1]
-    result = ''
-    for digit in num:
-        if int(digit) != 0:
-            result += digit
-    return int(result[::-1])
+    num = str(num)
+    i = 0
+    while i < len(num):
+        if num[-1] == '0':
+            num = num[:-1]
+        i += 1
+    return num
+
+
+# print(print(zeros(300400)))
 
 
 # print(zeros(3400))
@@ -49,3 +61,15 @@ def rec_sum_of_digits(num):
 
 
 # print(rec_sum_of_digits(132189))
+
+
+def highest_common_divisor(n1, n2):
+    result = []
+    i = 1
+    while i <= max(n1, n2):
+        if n1 % i == 0 and n2 % i == 0:
+            result.append(i)
+        i += 1
+    return *result,
+
+# print(highest_common_divisor(24, 56))
